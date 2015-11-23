7c931bb7a9dc7249c6c92c6443540d81712a894f
dmitryame <dmitry@custommode.com>
2015-11-23 13:52:44 -0500

    progress

--- TenTags/cloud/main.js
+++ TenTags/cloud/main.js
@@ -2,22 +2,34 @@ var HashTag = Parse.Object.extend("HashTag");
 
 // Check if stopId is set, and enforce uniqueness based on the stopId column.
 Parse.Cloud.beforeSave("HashTag", function(request, response) {
-  if (!request.object.get("hashTag")) {
-    response.error('A hashTag must have a value.');
-  } else {
-    var query = new Parse.Query(HashTag);
-    query.equalTo("hashTag", request.object.get("hashTag"));
-    query.first({
-      success: function(object) {
-        if (object) {
-          response.error("A HashTag with this value already exists.");
-        } else {
-          response.success();
+
+
+//first delete all previousy tags associated with this post
+    query = new Parse.Query(HashTag);
+      var tagString = request.object.get("hashTag");
+      query.equalTo("hashTag", tagString);
+      query.find({
+        success: function(myhashtags) {
+          console.log("found " + myhashtags.length + " hashTags to delete");
+          Parse.Object.destroyAll(myhashtags, {
+            success: function() {
+                // and now intert the new tag
+                    // Create a new instance of that class.
+                    var hashTag = new HashTag();
+                    hashTag.set("hashTag", tagString);
+                    hashTag.save();
+	                response.success();
+            },
+            error: function(error) {
+                console.error("Error deleting related HashTags " + error.code + ": " + error.message);
+                response.error();
+            }
+          });
+        },
+        error: function(error) {
+            console.error("Error finding related hashtags " + error.code + ": " + error.message);
+            response.error();
         }
-      },
-      error: function(error) {
-        response.error("Could not validate uniqueness for this HashTag object.");
-      }
-    });
-  }
+      });
+
 });
