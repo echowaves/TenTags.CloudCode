d3c83316a61932f53dbde58c546aed2e2bb88384
dmitryame <dmitry@custommode.com>
2015-12-01 21:22:13 -0500

    layer module added

--- TenTags/cloud/main.js
+++ TenTags/cloud/main.js
@@ -1,3 +1,23 @@
+var fs = require('fs');
+var layer = require('cloud/layer-parse-module/layer-module.js');
+
+
+var layerProviderID = 'layer:///providers/dbdb9d0a-9772-11e5-bdbe-3a8a16005a40';  // Should have the format of layer:///providers/<GUID>
+var layerKeyID = 'layer:///keys/b96eef8c-989a-11e5-98d4-6ac90b000c5d';   // Should have the format of layer:///keys/<GUID>
+var privateKey = fs.readFileSync('cloud/layer-parse-module/keys/layer-key.js');
+layer.initialize(layerProviderID, layerKeyID, privateKey);
+
+Parse.Cloud.define("generateToken", function(request, response) {
+    var currentUser = request.user;
+    if (!currentUser) throw new Error('You need to be logged in!');
+    var userID = currentUser.id;
+    var nonce = request.params.nonce;
+    if (!nonce) throw new Error('Missing nonce parameter');
+        response.success(layer.layerIdentityToken(userID, nonce));
+});
+
+
+
 var HashTag = Parse.Object.extend("HashTag");
 
 // Check if stopId is set, and enforce uniqueness based on the stopId column.
